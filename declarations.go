76603a2ec83b4adbb026f0d2808846983711ffd9
Lucas <lucas@MacBook-Air-de-Lucas.local>
2022-09-30 11:12:16 -0300

    feat: declarations

---
+++ declarations.go
@@ -0,0 +1,12 @@
+package declarations
+
+
+var (
+	b bool = true
+	c int = 10
+)
+
+func test(){
+	b = true
+	println(b)
+}
---
+++ variables.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+
+var (
+	name string
+	n1 int
+	n2 int32
+)
+
+func main(){
+	name = "Lucas"
+	fmt.Println("Hello", name)
+}
