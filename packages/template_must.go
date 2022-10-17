6005dc4937368f73273bac5eb0fc0ebf50e4e2b1
Lucas <lucas@MacBook-Air-de-Lucas.local>
2022-10-17 11:23:36 -0300

    templates

---
+++ packages/template_must.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"html/template"
+	"os"
+)
+
+type Curso struct {
+	Nome 						string
+	CargaHoraria 		int
+}
+func main()  {
+	curso := Curso{"Go", 40}
+	t := template.Must(template.New("TemplateCurso").Parse("Curso: {{.Nome}} = Carga Horária: {{.CargaHoraria}}"))
+
+	err := t.Execute(os.Stdout, curso)
+	if err != nil {
+		panic(err)
+	}
+}
